94b16fd691fe61984b7da97fc5d9289d3b476fc2
twothreeMin <foreverything1023@gmail.com>
2023-07-03 19:27:33 +0900

    [add] signin, signup 페이지

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import logo from './logo.svg';
-import React, {useState, useEffect} from 'react';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ErrorPage from "./page/error-page";
+import LoginPage from "./page/login";
+import SignupPage from "./page/signup";
 
 function App() {
-  const [message, setMessage] = useState(["test", "test2"]);
-
-  useEffect(() => {
-    fetch("/test")
-        .then((response) => response.json())
-        .then((json) => setMessage(json.SUCCESS_TEXT));
-}, []);
-
   return (
     <div className="App">
-      <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          {message}
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/home" element={<LoginPage />} />
+          <Route path="/signin" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
